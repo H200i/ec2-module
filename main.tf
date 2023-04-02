640c82675140b001cf000aa378c4f2f482f9bc4b
H200i <101483594+H200i@users.noreply.github.com>
2023-04-03 00:30:05 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,11 +6,4 @@ resource "aws_instance" "my_ec2_first" {
   }
 }
 
-resource "aws_instance" "my_ec2" {
-  ami           = "ami-0557a15b87f6559cf"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "hassw"
-  }
-}
 
